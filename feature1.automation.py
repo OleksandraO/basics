07b97be29c82240cc490f0cf10e97a3b708098e0
OleksandraO <oleksandra.orellana@gmail.com>
2022-11-18 21:50:20 -0500

    feature 1 test scenario added

--- feature1.automation.py
+++ feature1.automation.py
@@ -2,3 +2,8 @@ print('automation feature1')
 print('scenario 1 is working')
 print('scenario 2 is working')
 print('feature 1 testing is completed')
+
+print('automation feature1')
+print('scenario 1 is working')
+print('scenario 2 is working')
+print('feature 1 testing is completed')
